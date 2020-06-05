32c9e06e7696edeba5d67553533912ea22ce5dd3
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-05 18:24:18 +0100

    FIX: Tests now passing for PHPLint

--- tests/AddPHPLinkTaskTest.php
+++ tests/AddPHPLinkTaskTest.php
@@ -3,7 +3,7 @@
 namespace Suilven\PHPTravisEnhancer\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Suilven\PHPTravisEnhancer\AddDuplicationCheckTask;
+use Suilven\PHPTravisEnhancer\AddPHPLintTask;
 
 class AddPHPLinkTaskTest extends TestCase
 {
@@ -23,23 +23,23 @@ class AddPHPLinkTaskTest extends TestCase
     {
         parent::tearDown();
 
-        $this->removeIfExists(self::TRAVIS_FILE);
+       $this->removeIfExists(self::TRAVIS_FILE);
     }
 
 
     public function testPHPLintEmptyTravisFile(): void
     {
-        $task = new AddDuplicationCheckTask();
+        $task = new AddPHPLintTask();
         $task->run(self::TRAVIS_FILE);
-        $this->assertExpectedFileContents(self::TRAVIS_FILE, 'testDuplicationEmptyTravisFile.yml');
+        $this->assertExpectedFileContents(self::TRAVIS_FILE, 'testAddPhpLintEmptyTravisFile.yml');
     }
 
 
     public function testPHPLintExistingTravisFile(): void
     {
         $this->copySampleTravisFileTo(self::TRAVIS_FILE);
-        $task = new AddDuplicationCheckTask();
+        $task = new AddPHPLintTask();
         $task->run(self::TRAVIS_FILE);
-        $this->assertExpectedFileContents(self::TRAVIS_FILE, 'testDuplicationExistingTravisFile.yml');
+        $this->assertExpectedFileContents(self::TRAVIS_FILE, 'testAddPhpLintExistingTravisFile.yml');
     }
 }
