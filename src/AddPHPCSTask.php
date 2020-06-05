ba9b7b69c9370e3d5b6cf1cae4e05e3c2a08bcdf
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-05 21:00:54 +0100

    ENHANCEMENT: PHPCS now can be added to travis file

---
+++ src/AddPHPCSTask.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Suilven\PHPTravisEnhancer;
+
+use Suilven\PHPTravisEnhancer\Abstraction\TaskBase;
+use Suilven\PHPTravisEnhancer\IFace\Task;
+
+class AddPHPCSTask extends TaskBase implements Task
+{
+    /** @return string bash variable for use in Travis script */
+    public function getFlag(): string
+    {
+        return 'PHPCS_TEST';
+    }
+
+
+    public function getBeforeScript(): ?string
+    {
+        return null;
+    }
+
+
+    public function getScript(): ?string
+    {
+        return 'vendor/bin/phpcs --standard=ruleset.xml --extensions=php --tab-width=4 -sp src tests';
+    }
+
+    // @todo Copy the ruleset.xml file
+}
