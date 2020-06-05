41f2e6af3d5769ac50947b75856edf3d8b1d8e0c
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-05 20:07:57 +0100

    ENHANCEMENT: Test for adding psalm

---
+++ src/AddPsalmTask.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Suilven\PHPTravisEnhancer;
+
+use Suilven\PHPTravisEnhancer\Abstraction\TaskBase;
+use Suilven\PHPTravisEnhancer\IFace\Task;
+
+class AddPsalmTask extends TaskBase implements Task
+{
+    /** @return string bash variable for use in Travis script */
+    public function getFlag(): string
+    {
+        return 'PSALM_TEST';
+    }
+
+
+    public function getBeforeScript(): ?string
+    {
+        return null;
+    }
+
+
+    public function getScript(): ?string
+    {
+        return './vendor/bin/psalm --show-info=true;';
+    }
+}
