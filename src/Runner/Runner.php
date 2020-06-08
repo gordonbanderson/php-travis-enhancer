1ad60ee179ce312ab3b6eaa87e97ab587b9c930c
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-08 22:58:11 +0100

    WIP: Switching to non integrated temporarily

---
+++ src/Runner/Runner.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace Suilven\PHPTravisEnhancer\Runner\Runner;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Runner
+{
+    public function run()
+    
+    }
+}
