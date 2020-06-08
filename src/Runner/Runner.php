31742808f0e017842000a0bd6e825c460907f662
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-08 23:03:13 +0100

    WIP: CLImate now outputting from binary script

--- src/Runner/Runner.php
+++ src/Runner/Runner.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types = 1);
 
-namespace Suilven\PHPTravisEnhancer\Runner\Runner;
+namespace Suilven\PHPTravisEnhancer\Runner;
 
+use League\CLImate\CLImate;
 use Symfony\Component\Yaml\Yaml;
 
 class Runner
 {
-    public function run()
-    
+    public function run() {
+        $climate = new CLImate();
+        $climate->backgroundCyan('YO!');
     }
 }
