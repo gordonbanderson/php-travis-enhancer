222d78d433c91b407ec7eb53fd756e15ec3ee670
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-08 22:45:27 +0100

    FIX: Refactor did not change namespace

--- src/Task/AddPHPStanTask.php
+++ src/Task/AddPHPStanTask.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Suilven\PHPTravisEnhancer;
+namespace Suilven\PHPTravisEnhancer\Task;
 
 use Suilven\PHPTravisEnhancer\Abstraction\TaskBase;
 use Suilven\PHPTravisEnhancer\IFace\Task;
